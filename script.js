50d5a253b948e876187465285f943ec47f024863
Pranab Bhowmik <109957935+Pranab09122002@users.noreply.github.com>
2023-12-24 15:45:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const createBtn = document.getElementById("btn");
+const notesContainer = document.querySelector(".notes-container");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes",notesContainer.innerHTML);
+}
+
+
+
+createBtn.addEventListener("click",()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "img/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+    inputBox.focus();
+    updateStorage();
+});
+
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    } else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach((nt) => {
+            nt.addEventListener('keyup', function () {
+                updateStorage();
+            });
+        });
+    }
+});
+
+
+document.addEventListener("keydoen", event =>{
+    if(event.key === "Enter"){
+        document.execCommand("inserLinerBreak");
+        event.preventDefault();
+    }
+})
